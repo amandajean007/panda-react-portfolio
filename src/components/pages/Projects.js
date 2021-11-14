39370c53293ef6470053b12ded2503f59bbd7852
Amanda McBee <ahanes35@yahoo.com>
2021-11-13 23:09:50 -0700

    projects page updated

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -26,87 +26,88 @@ export default function Projects() {
   }
 
   return (
-    <div id="projectLinks" class="content">
+    <div className="content">
 
-    <div class="paragraph">
-      <h3>Password Generator</h3>
-      <a href="https://amandajean007.github.io/Password-Generator/" target="_blank" rel="noreferrer" id="1">
-        <img alt="Password Generator" src={password} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Password-Generator" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Password Generator</h3>
+        <a href="https://amandajean007.github.io/Password-Generator/" target="_blank" rel="noreferrer" id="1">
+          <img alt="Password Generator" src={password} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Password-Generator" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>Code Quiz</h3>
-      <a href="https://amandajean007.github.io/Code-Quiz/" target="_blank" rel="noreferrer" id="2">
-        <img class="photo" alt="Code Quiz" src={codequiz} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Code-Quiz" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Code Quiz</h3>
+        <a href="https://amandajean007.github.io/Code-Quiz/" target="_blank" rel="noreferrer" id="2">
+          <img class="photo" alt="Code Quiz" src={codequiz} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Code-Quiz" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>Work Day Scheduler</h3>
-      <a href="https://amandajean007.github.io/Work-Day-Scheduler/" target="_blank" rel="noreferrer" id="3">
-        <img class="photo" alt="Work Day Scheduler" src={scheduler} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Work-Day-Scheduler" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Work Day Scheduler</h3>
+        <a href="https://amandajean007.github.io/Work-Day-Scheduler/" target="_blank" rel="noreferrer" id="3">
+          <img class="photo" alt="Work Day Scheduler" src={scheduler} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Work-Day-Scheduler" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>Recipe Generator</h3>
-      <a href="https://amandajean007.github.io/AAA/" target="_blank" rel="noreferrer" id="4">
-        <img class="photo" alt="recipe generator" src={foodGenerator} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/AAA" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
-    <div class="paragraph">
-      <h3>Weather Tracker</h3>
-      <a href="https://amandajean007.github.io/Weather-Tracker/ " target="_blank" rel="noreferrer" id="5">
-        <img class="photo" alt="Weather Tracker" src={weather} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Weather-Tracker" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Recipe Generator</h3>
+        <a href="https://amandajean007.github.io/AAA/" target="_blank" rel="noreferrer" id="4">
+          <img class="photo" alt="recipe generator" src={foodGenerator} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/AAA" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>DinDin - Recipe Warehouse</h3>
-      <a href="https://secure-crag-98297.herokuapp.com/login" target="_blank" rel="noreferrer" id="6">
-        <img class="photo" alt="DinDin" src={dindin} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Din-Din" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Weather Tracker</h3>
+        <a href="https://amandajean007.github.io/Weather-Tracker/ " target="_blank" rel="noreferrer" id="5">
+          <img class="photo" alt="Weather Tracker" src={weather} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Weather-Tracker" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>Technology Blog</h3>
-      <a href="https://whispering-brushlands-45251.herokuapp.com/" target="_blank" rel="noreferrer" id="7">
-        <img class="photo" alt="TechBlog" src={techblog} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Tech-Blog" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>DinDin - Recipe Warehouse</h3>
+        <a href="https://secure-crag-98297.herokuapp.com/login" target="_blank" rel="noreferrer" id="6">
+          <img class="photo" alt="DinDin" src={dindin} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Din-Din" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>Fitness Tracker</h3>
-      <a href="https://safe-spire-01308.herokuapp.com/" target="_blank" rel="noreferrer" id="8">
-        <img class="photo" alt="FitnessTracker" src={fitness} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Fitness-Tracker" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Technology Blog</h3>
+        <a href="https://whispering-brushlands-45251.herokuapp.com/" target="_blank" rel="noreferrer" id="7">
+          <img class="photo" alt="TechBlog" src={techblog} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Tech-Blog" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>Note Taker</h3>
-      <a href="https://panda-note-taker.herokuapp.com/" target="_blank" rel="noreferrer" id="9">
-        <img class="photo" alt="NoteTaker" src={notetaker} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Note-Taker" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Fitness Tracker</h3>
+        <a href="https://safe-spire-01308.herokuapp.com/" target="_blank" rel="noreferrer" id="8">
+          <img class="photo" alt="FitnessTracker" src={fitness} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Fitness-Tracker" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-    <div class="paragraph">
-      <h3>Offline-Online Budget Tracker</h3>
-      <a href="https://cryptic-tundra-70283.herokuapp.com/" target="_blank" rel="noreferrer" id="10">
-        <img class="photo" alt="OfflineOnlineBudgetTracker" src={budget} width="80%" height="auto"></img>
-      </a>
-      <a href="https://github.com/amandajean007/Online-Offline-Budget-Tracker" target="_blank" rel="noreferrer">Check out the code!</a>
-    </div>
+      <div class="paragraph">
+        <h3>Note Taker</h3>
+        <a href="https://panda-note-taker.herokuapp.com/" target="_blank" rel="noreferrer" id="9">
+          <img class="photo" alt="NoteTaker" src={notetaker} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Note-Taker" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
 
-  </div>
+      <div class="paragraph">
+        <h3>Offline-Online Budget Tracker</h3>
+        <a href="https://cryptic-tundra-70283.herokuapp.com/" target="_blank" rel="noreferrer" id="10">
+          <img class="photo" alt="OfflineOnlineBudgetTracker" src={budget} width="80%" height="auto"></img>
+        </a><br />
+        <a href="https://github.com/amandajean007/Online-Offline-Budget-Tracker" target="_blank" rel="noreferrer">Check out the code!</a>
+      </div>
+
+    </div>
 );
 }
