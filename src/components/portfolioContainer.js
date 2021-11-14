ce9f0e4057ab4c54bba82e718cd865ead74c5507
Amanda McBee <ahanes35@yahoo.com>
2021-11-13 21:03:06 -0700

    creating the react pages the way it is supposed to be

---
+++ src/components/portfolioContainer.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Footer from './Footer';
+import Header from './Header';
+import './PortfolioContainer.css';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div className="main">
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      <div className="portfolio-container">{renderPage()}</div>
+      <Footer />
+    </div>
+  );
+}
